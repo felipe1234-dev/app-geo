17c0475bda1d6d50ca5df0688f19ccb7661b78e2
felipe1234-dev <felipejean2002@gmail.com>
2021-10-15 18:48:30 -0300

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('app-geo');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app-geo app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'app-geo';
+
+export class AppComponent implements OnInit {
+  public title = "App Geo";
+
+  public mapURL: string = "";
+  public accAllowed: boolean = true;
+
+  public config = {
+    maximumAge: 60000, 
+    timeout: 20000, 
+    enableHighAccuracy:true
+  }
+
+  public coords = {
+    lat: 0,
+    long: 0
+  }
+
+  updateCoords(coords: { latitude: number, longitude: number }): void {
+    const { latitude, longitude } = coords;
+
+    this.coords.lat = latitude;
+    this.coords.long = longitude;
+    this.mapURL = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+    
+    console.log()
+  }
+
+  getLocal(): void {
+    const geo = navigator.geolocation;
+
+    if (!geo) {
+      return;
+    }
+
+    geo.getCurrentPosition(
+      pos => this.updateCoords(pos.coords),
+      () => this.accAllowed = false,
+      this.config
+    );
+
+    geo.watchPosition(
+      pos => this.updateCoords(pos.coords), 
+      () => this.accAllowed = false,
+      this.config
+    );
+
+  }
+
+  ngOnInit() {
+    this.getLocal();
+  }
 }
